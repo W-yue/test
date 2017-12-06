b2c9b41f91a2cff7d50a9fd077abd236f3e46a36
W-yue <jinhong212@163.com>
2017-12-06 13:53:54 +0800

    wu

--- Jin_practice/js/department.js
+++ Jin_practice/js/department.js
@@ -1,6 +1,29 @@
 var app = angular.module("myApp",['ui.router']);
+// app.run(function($q,$timeout){
+//     var defer = $q.defer();
+//     $timeout(function(){
+//         defer.resolve("ok");
+//     },2000);
 
-app.config(function($stateProvider){
+//     defer.promise
+//     .then(function(msg){
+//         console.log(msg);
+//         var defer1 = $q.defer();
+//         $timeout(function(){
+//             defer1.resolve("ok1")
+//         },2000);
+//         return defer1.promise;
+//     })
+//     .then(function(msg1){
+//         console.log(msg1);
+//     });   
+// });
+
+app.config(function($stateProvider,$controllerProvider){
+    //异步加载
+    app.register = {};
+    app.register.controller = $controllerProvider.register;
+
     var one = {
         name:'111',
         url:'/111',
@@ -9,7 +32,17 @@ app.config(function($stateProvider){
     var two = {
         name:'222',
         url:'/222',
-        templateUrl:"department.html"
+        templateUrl:"department.html",
+        //controller:'myCtrl',
+        resolve:{
+            loadJs:function($q){
+                var defer =  $q.defer();
+                $script(["js/myctrl.js"],function(){
+                    defer.resolve();
+                });
+                return defer.promise;
+            } 
+        }
     }
     var three = {
         name:'333',
@@ -20,103 +53,4 @@ app.config(function($stateProvider){
     $stateProvider.state(one);
     $stateProvider.state(two);
     $stateProvider.state(three);
-});
-
-
-app.controller("myCtrl", function ($scope, $http,$filter) { 
-    $scope.controller = {
-        //flag:false,
-        list:[],
-        index:null,
-        info:null,
-        keyword:"",
-        search:function(e){           
-            if(e.keyCode==13){//键盘enter对应数字13
-                this.get();
-            }
-        },
-        //得到关键字
-        getSearchData:function(){
-            var data = {
-                keyword:this.keyword,
-                time:new Date().getTime()
-            }
-            if(/[0-9]/.test(this.keyword)){
-                alert("不能输入数字");
-                return false;
-            }      
-            this.searchData = data; //保存一下原来的数据
-            return data;
-        },
-        //数据处理
-        dealSearchData:function(data){
-            this.list = $filter('filter')(data,this.searchData.keyword);
-
-        },
-        get:function(){ 
-            var searchData = this.getSearchData();
-            if(!searchData) return;
-            $http.get("./json/department.json",{
-                params:searchData
-            }).success(function (data) {
-                this.dealSearchData(data.department);
-            }.bind(this));         
-        },
-        
-        //模态框
-        modal:function(){
-            $('#info')
-            .modal({closable:false})
-            .modal("toggle");         
-        },
-        //添加
-        add:function(){
-            this.modal();              
-        },
-        //修改
-        modify:function($index){
-            this.index = $index;
-            this.info = angular.copy(this.list[this.index]);
-            this.modal();
-        },
-        //删除
-        delete: function ($index) {
-            this.index = $index;
-            if (confirm("确定删除？")) {
-                this.list.splice(this.index, 1);
-            }
-            this.index = null;
-        },
-        //保存
-        save: function () {
-            if (this.index == null) {
-                //新增信息   
-                //判断是否填入完整  
-                var count = 0;
-                for (x in this.info) {
-                    count++;
-                }
-                if (count == 4) {
-                    this.list.push(this.info);
-                    this.info = null;
-                    this.modal();
-                }else{
-                    confirm("请输入完整信息！");
-                }
-                count = 0;
-            } else {
-                //修改信息
-                this.list[this.index] = this.info;
-                this.info = null;
-                this.index = null;
-                this.modal();
-            }
-        }
-    }
-    $scope.controller.get();
-  
-//     $scope.$watch("keyword",function (value) {
-//         $scope.search=$scope.keyword;
-//    }) 
-
 });
